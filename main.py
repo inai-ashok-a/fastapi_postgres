00608b7cf290e9a7247dac091b0fe15cbc2cedf2
Ashok <ashok.a@inai.io>
2022-07-27 18:27:27 +0530

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends
-# from sqlalchemy.orm import Session
-# from config.db import get_db
-# from models.users import User
-# from schemas.schema_user import User_req
+from sqlalchemy.orm import Session
+from config.db import get_db
+from models.users import User
+from schemas.schema_user import User_req
 
 app = FastAPI()
 
@@ -10,8 +10,8 @@ app = FastAPI()
 def root():
     return "Hello heroku"
 
-# @app.get("/api/users")
-# def get_by_id( db: Session = Depends(get_db)):
-#     #return db.query(User).filter(User.id == id).first()
-#     return db.query(User).all()
-#
+@app.get("/api/users")
+def get_by_id( db: Session = Depends(get_db)):
+    #return db.query(User).filter(User.id == id).first()
+    return db.query(User).all()
+
