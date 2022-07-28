73876b16dd7764c77eb99bcd5fcb2783e0c8f363
Ashok <ashok.a@inai.io>
2022-07-28 12:04:59 +0530

    function name changed

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def root():
     return "Hello heroku from Ashok!!!"
 
 @app.get("/api/users")
-def get_by_id( db: Session = Depends(get_db)):
+def get_all_users( db: Session = Depends(get_db)):
     #return db.query(User).filter(User.id == id).first()
     return db.query(User).all()
 
 
 @app.post('/items',status_code=status.HTTP_201_CREATED)
-def create_an_item( obj:User_req,db: Session = Depends(get_db)):
+def create_an_users( obj:User_req,db: Session = Depends(get_db)):
 
      db_item = db.query(User).filter(User.email == obj.email).first()
 
